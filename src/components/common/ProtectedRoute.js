95ede9009c3ccede77baf088483edccf76fc62cc
Frankie Riviera <frankgriviera@outlook.com>
2020-12-01 01:56:28 -0500

    removed comments and console.log in Protected route

--- src/components/common/ProtectedRoute.js
+++ src/components/common/ProtectedRoute.js
@@ -3,32 +3,14 @@ import { Route, Redirect } from 'react-router-dom'
 import Auth from '../../auth/auth'
 
 export const ProtectedRoute = ({ component: Component, ...rest }) => {
-
-    console.log("ProtectedRoute Component: ", Component)
-    console.log("ProtectedRoute THE REST: ", rest)
     return (
         <Route
             {...rest}
             render={props => {
-                console.log("ProtectedRoute props:",props)
                 let token = sessionStorage.getItem('token');
-
-                // console.log("ProtectedRoute before check Auth.isAuthenticated: ", Auth.isAuthenticated//)
-                // if (token != null) {
-                //     // console.log("ProtectedRoute Oh Look a token!! ", Auth.isAuthenticated)
-                //     Auth.check_Authenticated().then((authenticated) => {
-                //         Auth.isAuthenticated = authenticated
-                //         // console.log("ProtectedRoute results after token check IN THEN ", Auth.isAuthenticated)
-                //     })
-                //     // console.log("ProtectedRoute results after token check ", Auth.isAuthenticated)//
-                // } else {
-                //     // console.log("ProtectedRoute No Token No Service ", Auth.isAuthenticated)
-                //     Auth.isAuthenticated = false
-                // }
                 if (token != null) {
                     Auth.isAuthenticated = true
                 }
-                // console.log("ProtectedRoute after check Auth.isAuthenticated: ", Auth.isAuthenticated)
                 if (Auth.isAuthenticated) {
                     return <Component {...props} />
                 } else {
@@ -42,44 +24,5 @@ export const ProtectedRoute = ({ component: Component, ...rest }) => {
         />
     );
 }
-
-// export const ProtectedRoute = async ({component:Component, ...rest}) => {
-
-//     let isAuthenticated = await Auth.isAuthenticated
-//     console.log("isAuthenticated: ",isAuthenticated)
-//     if(isAuthenticated){
-//         return (
-//             <Route 
-//                 {...rest}
-//                 render ={ props =>{
-//                     let token = sessionStorage.getItem('token');
-//                     if (token != null) {
-//                         Auth.isAuthenticated=true;
-//                     }
-//                         return <Component {...props}/>
-
-//                 }
-//             }
-//             />
-//         );
-//     }else{
-//         return (
-//             <Route 
-//                 {...rest}
-//                 render ={ props =>{
-//                     let token = sessionStorage.getItem('token');
-//                     if (token != null) {
-//                         Auth.isAuthenticated=true;
-//                     }
-//                         return <Redirect 
-//                         to="/"
-//                         />
-//                 }
-//             }
-//             />
-//         );
-//     }
-
-// } 
 
 export default ProtectedRoute;
